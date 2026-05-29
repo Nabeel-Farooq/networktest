cf4474909f8f54b86b64692e99491dc800ac5218
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-29 14:25:18 +0500

    Update downloadtester.py

--- bandwidth/downloadtester.py
+++ bandwidth/downloadtester.py
@@ -1,95 +1,192 @@
-# !/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 
-"""
-.. codeauthor:: Juan Luis Baptiste <juan.baptiste@gmail.com>
-"""
-
-import csv
 import os
-import requests
 import sys
 import time
+from pathlib import Path
+from tempfile import gettempdir
+
+import requests
 
 import common
 
 
-class DownloadTester():
-    """Class to do download speed tests."""
+class DownloadTester:
+    """Class to perform download speed tests."""
 
     DEFAULT_LOCATION = "use"
     DEFAULT_DOWNLOAD_COUNT = 1
     VERBOSE = True
-    localFilename = "100MB-newark.bin"
-    locations = {
-     'london': 'http://speedtest.london.linode.com/100MB-london.bin',
-     'sanjose': 'http://speedtest.sjc01.softlayer.com/speedtest/speedtest/random500x500.jpg' ,
-     'tokyo': 'http://speedtest.tokyo.linode.com/100MB-tokyo.bin',
-     'use': 'http://speedtest.newark.linode.com/100MB-newark.bin',
-     'usw': 'http://speedtest.fremont.linode.com/100MB-fremont.bin',
-     'washington': 'http://speedtest.wdc01.softlayer.com/downloads/test500.zip'
+
+    LOCATIONS = {
+        "london": (
+            "http://speedtest.london.linode.com/"
+            "100MB-london.bin"
+        ),
+        "sanjose": (
+            "http://speedtest.sjc01.softlayer.com/"
+            "speedtest/speedtest/random500x500.jpg"
+        ),
+        "tokyo": (
+            "http://speedtest.tokyo.linode.com/"
+            "100MB-tokyo.bin"
+        ),
+        "use": (
+            "http://speedtest.newark.linode.com/"
+            "100MB-newark.bin"
+        ),
+        "usw": (
+            "http://speedtest.fremont.linode.com/"
+            "100MB-fremont.bin"
+        ),
+        "washington": (
+            "http://speedtest.wdc01.softlayer.com/"
+            "downloads/test500.zip"
+        ),
     }
-    overall_time_elapsed = 0
-    __size = 0
+
+    def __init__(self):
+        self.local_filename = "100MB-newark.bin"
+        self.overall_time_elapsed = 0
+        self._size = 0
 
     def get_filesize(self):
-        """Return file size attribute."""
-        return self.__size
+        """Return downloaded file size."""
+
+        return self._size
 
     def get_location(self, location=None):
-        """Return test file download location.
+        """
+        Return test file download URL.
 
         Arguments:
-        url -- Location key for locations array.
+        location -- Location key
         """
-        if location is None:
-            location = self.DEFAULT_LOCATION
-        return self.locations.get(location)
 
-    def get_local_filename(self, url):
-        """Return file name from download URL."""
-        return url.split('/')[-1]
+        return self.LOCATIONS.get(
+            location or self.DEFAULT_LOCATION
+        )
+
+    @staticmethod
+    def get_local_filename(url):
+        """Extract local filename from URL."""
+
+        return url.rsplit("/", 1)[-1]
 
     def download_file(self, url):
-        """Download a file and save it to a temp directory.
+        """
+        Download file and return speed.
 
         Arguments:
-        url -- URL to file to download.
+        url -- File URL
         """
-        self.localFilename = self.get_local_filename(url)
-        with open('/tmp/' + self.localFilename, 'wb') as f:
-            self.__size = 0
-            dl_speed = 0
-            # start = time.clock()
-            start = time.mktime(time.localtime())
-            r = requests.get(url, stream=True)
-            total_length = r.headers.get('content-length')
 
-            if total_length is None:  # no content length header
-                f.write(r.content)
-            else:
-                for chunk in r.iter_content(1024):
-                    self.__size += len(chunk)
-                    f.write(chunk)
-                    done = int(50 * self.__size / int(total_length))
-                    # time_elapsed = (time.clock() - start)
-                    time_elapsed = (time.mktime(time.localtime()) - start)
-                    if time_elapsed > 0:
-                        dl_speed = self.__size/time_elapsed
-                        self.overall_time_elapsed = time_elapsed
+        self.local_filename = (
+            self.get_local_filename(url)
+        )
 
-                    if self.VERBOSE:
-                        # Convert to MB/s and Mbps for printing
-                        sys.stdout.write("\r[%s%s] %s MB/s - %s Mbps" %
-                                         ('=' * done, ' ' * (50-done),
-                                          round(dl_speed*common.SPEED_MB_SEC, 2),
-                                          round(dl_speed*common.SPEED_MBIT_SEC, 2)))
-                        sys.stdout.flush()
+        temp_path = (
+            Path(gettempdir()) / self.local_filename
+        )
 
-                self.cleanup()
-        return dl_speed
+        self._size = 0
+        download_speed = 0
 
-    def cleanup(self):
-        """Cleanup test, delete downloaded file."""
-        # Cleanup
-        os.remove("/tmp/" + self.localFilename)
+        start = time.time()
+
+        try:
+            with requests.get(
+                url,
+                stream=True,
+                timeout=30,
+            ) as response:
+
+                response.raise_for_status()
+
+                total_length = int(
+                    response.headers.get(
+                        "content-length",
+                        0,
+                    )
+                )
+
+                with temp_path.open("wb") as file:
+
+                    for chunk in response.iter_content(
+                        chunk_size=8192
+                    ):
+
+                        if not chunk:
+                            continue
+
+                        chunk_len = len(chunk)
+
+                        self._size += chunk_len
+
+                        file.write(chunk)
+
+                        time_elapsed = (
+                            time.time() - start
+                        )
+
+                        if time_elapsed > 0:
+                            download_speed = (
+                                self._size / time_elapsed
+                            )
+
+                            self.overall_time_elapsed = round(
+                                time_elapsed,
+                                2,
+                            )
+
+                        if (
+                            self.VERBOSE
+                            and total_length > 0
+                        ):
+                            done = int(
+                                50
+                                * self._size
+                                / total_length
+                            )
+
+                            sys.stdout.write(
+                                "\r[%s%s] %s MB/s - %s Mbps"
+                                % (
+                                    "=" * done,
+                                    " " * (50 - done),
+                                    round(
+                                        download_speed
+                                        * common.SPEED_MB_SEC,
+                                        2,
+                                    ),
+                                    round(
+                                        download_speed
+                                        * common.SPEED_MBIT_SEC,
+                                        2,
+                                    ),
+                                )
+                            )
+
+                            sys.stdout.flush()
+
+        finally:
+            self.cleanup(temp_path)
+
+        return download_speed
+
+    @staticmethod
+    def cleanup(filepath):
+        """
+        Delete downloaded temporary file.
+
+        Arguments:
+        filepath -- File path to remove
+        """
+
+        try:
+            if filepath.exists():
+                filepath.unlink()
+
+        except OSError:
+            pass
