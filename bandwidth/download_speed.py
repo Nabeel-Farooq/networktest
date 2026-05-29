bb002f223dd3f2c457197023444a3b40f5ad670e
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-29 14:26:12 +0500

    Update download_speed.py

--- bandwidth/download_speed.py
+++ bandwidth/download_speed.py
@@ -1,194 +1,365 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 
-"""
-.. codeauthor:: Juan Luis Baptiste <juan.baptiste@gmail.com>
-"""
 
-# Inspired on netspeed.sh script from https://bitbucket.org/rsvp/gists/src
-
-# from __future__ import print_function
+# Inspired by netspeed.sh script from:
+# https://bitbucket.org/rsvp/gists/src
 
 import argparse
-import numpy
 import os
-import pkg_resources
 import signal
 import statistics
 import sys
 import time
-import urllib2
+from pathlib import Path
+
+import requests
 
 import common
-import downloadtester
 import csv_parser
+import downloadtester
+
+try:
+    from importlib.metadata import version as get_version
+except ImportError:
+    from importlib_metadata import version as get_version
+
 
 tester = downloadtester.DownloadTester()
+
 try:
-    version = pkg_resources.require("network-tests")[0].version
-except pkg_resources.DistributionNotFound:
-    version = "dev"
+    VERSION = get_version("network-tests")
+except Exception:
+    VERSION = "dev"
 
 
 def parse_option():
     """Parse command line arguments."""
+
     parser = argparse.ArgumentParser()
+
     parser.add_argument(
-        "-c", "--count", required=False, help="Number of downloads to do. \
-        Default: " + str(tester.DEFAULT_DOWNLOAD_COUNT))
+        "-c",
+        "--count",
+        type=int,
+        default=tester.DEFAULT_DOWNLOAD_COUNT,
+        help=(
+            "Number of downloads to do. "
+            f"Default: {tester.DEFAULT_DOWNLOAD_COUNT}"
+        ),
+    )
+
     parser.add_argument(
-        "-l", "--location", required=False, help="Server location for the \
-        test. Default: " + str(tester.DEFAULT_LOCATION),
-        choices=tester.locations)
+        "-l",
+        "--location",
+        choices=tester.LOCATIONS.keys(),
+        default=tester.DEFAULT_LOCATION,
+        help=(
+            "Server location for the test. "
+            f"Default: {tester.DEFAULT_LOCATION}"
+        ),
+    )
+
     parser.add_argument(
-        "-o", "--outfile", required=False, help="Destination file for test \
-        results in CSV format")
+        "-o",
+        "--outfile",
+        help="Destination CSV file for test results",
+    )
+
     parser.add_argument(
-        "-s", "--silent", action="store_true", help="Don't print verbose \
-        output from the download process")
+        "-s",
+        "--silent",
+        action="store_true",
+        help="Disable verbose output",
+    )
+
     parser.add_argument(
-        "-u", "--url", required=False, help="Alternate download URL (it must \
-        include path and filename)")
+        "-u",
+        "--url",
+        help=(
+            "Alternate download URL "
+            "(must include path and filename)"
+        ),
+    )
+
     parser.add_argument(
-        "-V", "--version", action="version", version="Program \
-        Version: " + version, help="Print program version")
+        "-V",
+        "--version",
+        action="version",
+        version=f"Program Version: {VERSION}",
+    )
 
     return parser.parse_args()
 
 
-def signal_handler(signal, frame):
-        """Signal handler."""
-        print '\n\nTest cancelled!\n'
-        tester.cleanup()
-        sys.exit(0)
+def signal_handler(sig, frame):
+    """Handle Ctrl+C."""
+
+    print("\n\nTest cancelled!\n")
+
+    sys.exit(0)
 
 
 signal.signal(signal.SIGINT, signal_handler)
 
 
+def get_verbose_printer(enabled):
+    """Return conditional print function."""
+
+    return print if enabled else lambda *a, **k: None
+
+
+def validate_url(url):
+    """Validate download URL."""
+
+    try:
+        response = requests.head(
+            url,
+            timeout=10,
+            allow_redirects=True,
+        )
+
+        return response.ok
+
+    except requests.RequestException:
+        return False
+
+
+def format_speed(speed):
+    """Format speed into MB/s and Mbps."""
+
+    return (
+        f"{round(speed * common.SPEED_MB_SEC, 2)}MB/s - "
+        f"{round(speed * common.SPEED_MBIT_SEC, 2)}Mbps"
+    )
+
+
 def main():
     """Run main program."""
-    # global functions.VERBOSE
+
     options = parse_option()
-    if options.silent:
-        tester.VERBOSE = False
 
-    if not options.silent:
-        def verboseprint(*args):
-            # Print each argument separately so caller doesn't need to
-            # stuff everything to be printed into a single string
-            for arg in args:
-                print arg,
-            print
-    else:
-        verboseprint = lambda *a: None      # do-nothing function
+    tester.VERBOSE = not options.silent
 
-    # Set download location
-    location = options.location or tester.DEFAULT_LOCATION
-    # Print the program version
-    verboseprint(os.path.basename(__file__) + ' ' + version + '\n')
+    verboseprint = get_verbose_printer(
+        tester.VERBOSE
+    )
 
-    # Overwrite with custom URL
-    if not options.url:
-        verboseprint('Location: ' + location)
-        url = tester.get_location(location)
-    else:
-        try:
-            urllib2.urlopen(options.url)
-        except Exception:
-            print "ERROR: Download URL does not exist."
-            sys.exit(1)
+    verboseprint(
+        f"{Path(__file__).name} v{VERSION}\n"
+    )
+
+    if options.url:
+
+        if not validate_url(options.url):
+            print(
+                "ERROR: Download URL does not exist."
+            )
+            return 1
+
         url = options.url
-    verboseprint('URL: ' + url)
 
-    # Check that output dir exists
-    if (
-        options.outfile and not
-        os.path.exists(os.path.dirname(options.outfile))
-       ):
-        print "\nERROR: Output file destination directory does not \
-        exist: " + os.path.dirname(options.outfile) + " \n"
-        sys.exit(1)
+    else:
+        location = options.location
 
-    num_tests = int(options.count or tester.DEFAULT_DOWNLOAD_COUNT)
-    verboseprint('Total Tests: ' + str(num_tests))
-    print
+        verboseprint(f"Location: {location}")
+
+        url = tester.get_location(location)
+
+    verboseprint(f"URL: {url}")
+
+    if options.outfile:
+
+        output_dir = os.path.dirname(
+            options.outfile
+        )
+
+        if output_dir and not os.path.exists(
+            output_dir
+        ):
+            print(
+                "\nERROR: Output file destination "
+                f"directory does not exist: "
+                f"{output_dir}\n"
+            )
+
+            return 1
+
+    verboseprint(
+        f"Total Tests: {options.count}\n"
+    )
+
+    print()
 
     results = []
-    n = 0
-    # Do tests
-    while (n < num_tests):
-        verboseprint('Test #' + str(n + 1) + ': ')
-        result = tester.download_file(url)
-        filesize = round(tester.get_filesize()/1024/1024, 2)
-        results.append(result)
-        verboseprint("\nDownloaded file size: " + str(filesize) + " MB")
-        verboseprint("\nAverage download speed: " +
-                     str(round(result*common.SPEED_MB_SEC, 2)) + " MB/s - " +
-                     str(round(result*common.SPEED_MBIT_SEC, 2)) + " Mbps\n")
-        n += 1
+    filesize = 0
 
-    overall_speed = sum(results)/n
+    for test_num in range(
+        1,
+        options.count + 1,
+    ):
+
+        verboseprint(f"Test #{test_num}:")
+
+        result = tester.download_file(url)
+
+        filesize = round(
+            tester.get_filesize() / 1024 / 1024,
+            2,
+        )
+
+        results.append(result)
+
+        verboseprint(
+            f"\nDownloaded file size: "
+            f"{filesize} MB"
+        )
+
+        verboseprint(
+            f"\nAverage download speed: "
+            f"{format_speed(result)}\n"
+        )
+
+    if not results:
+        print("No download results available.")
+        return 1
+
+    overall_speed = statistics.mean(results)
+
     median_speed = statistics.median(results)
-    deviation = numpy.std(results)
+
+    deviation = (
+        statistics.stdev(results)
+        if len(results) > 1
+        else 0
+    )
+
     min_speed = min(results)
     max_speed = max(results)
+
     verboseprint("\nTest Results:")
     verboseprint("---- -------\n")
-    verboseprint("Time Elapsed: " + str(tester.overall_time_elapsed) +
-                 " seconds\n")
-    verboseprint("Overall Average Download Speed: " +
-                 str(round(overall_speed*common.SPEED_MB_SEC, 2)) +
-                 "MB/s - " +
-                 str(round(overall_speed*common.SPEED_MBIT_SEC, 2)) + "Mbps")
-    verboseprint("Maximum download speed: " +
-                 str(round(max_speed*common.SPEED_MB_SEC, 2)) + "MB/s - " +
-                 str(round(max_speed*common.SPEED_MBIT_SEC, 2)) + "Mbps")
-    verboseprint("Minimum download speed: " +
-                 str(round(min_speed*common.SPEED_MB_SEC, 2)) + "MB/s - " +
-                 str(round(min_speed*common.SPEED_MBIT_SEC, 2)) + "Mbps")
-    verboseprint("Median download speed: " +
-                 str(round(median_speed*common.SPEED_MB_SEC, 2)) + "MB/s - " +
-                 str(round(median_speed*common.SPEED_MBIT_SEC, 2)) + "Mbps")
-    verboseprint("Standard Deviation: " +
-                 str(round(deviation*common.SPEED_MB_SEC, 2)) + "MB/s - " +
-                 str(round(deviation*common.SPEED_MBIT_SEC, 2)) + "Mbps\n")
 
-    # Create csv with test results
+    verboseprint(
+        f"Time Elapsed: "
+        f"{tester.overall_time_elapsed} seconds\n"
+    )
+
+    verboseprint(
+        f"Overall Average Download Speed: "
+        f"{format_speed(overall_speed)}"
+    )
+
+    verboseprint(
+        f"Maximum download speed: "
+        f"{format_speed(max_speed)}"
+    )
+
+    verboseprint(
+        f"Minimum download speed: "
+        f"{format_speed(min_speed)}"
+    )
+
+    verboseprint(
+        f"Median download speed: "
+        f"{format_speed(median_speed)}"
+    )
+
+    verboseprint(
+        f"Standard Deviation: "
+        f"{format_speed(deviation)}\n"
+    )
+
     if options.outfile:
-        scriptDir = os.getcwd()
-        csv_file = os.path.join(scriptDir, options.outfile)
-        date = time.strftime("%c")
-        overall_headers = ["Date",
-                           "URL",
-                           "Size (MB)",
-                           "Min (MB/s)",
-                           "Min (Mbps)",
-                           "Max (MB/s)",
-                           "Max (Mbps)",
-                           "Average (MB/s)",
-                           "Average (Mbps)",
-                           "Median (MB/s)",
-                           "Median (Mbps)",
-                           "Deviation (MB/s)",
-                           "Deviation (Mbps)",
-                           "Program Version"]
-        overall_values = [date,
-                          url,
-                          filesize,
-                          round(min_speed*common.SPEED_MB_SEC, 2),
-                          round(min_speed*common.SPEED_MBIT_SEC, 2),
-                          round(max_speed*common.SPEED_MB_SEC, 2),
-                          round(max_speed*common.SPEED_MBIT_SEC, 2),
-                          round(overall_speed*common.SPEED_MB_SEC, 2),
-                          round(overall_speed*common.SPEED_MBIT_SEC, 2),
-                          round(median_speed*common.SPEED_MB_SEC, 2),
-                          round(median_speed*common.SPEED_MBIT_SEC, 2),
-                          round(deviation*common.SPEED_MB_SEC, 2),
-                          round(deviation*common.SPEED_MBIT_SEC, 2),
-                          "v" + version]
-        overall = (overall_headers, overall_values)
-        csv_parser.csv_parser(results, csv_file, overall, filesize)
+
+        csv_file = os.path.join(
+            os.getcwd(),
+            options.outfile,
+        )
+
+        overall_headers = [
+            "Date",
+            "URL",
+            "Size (MB)",
+            "Min (MB/s)",
+            "Min (Mbps)",
+            "Max (MB/s)",
+            "Max (Mbps)",
+            "Average (MB/s)",
+            "Average (Mbps)",
+            "Median (MB/s)",
+            "Median (Mbps)",
+            "Deviation (MB/s)",
+            "Deviation (Mbps)",
+            "Program Version",
+        ]
+
+        overall_values = [
+            time.strftime("%c"),
+            url,
+            filesize,
+            round(
+                min_speed * common.SPEED_MB_SEC,
+                2,
+            ),
+            round(
+                min_speed * common.SPEED_MBIT_SEC,
+                2,
+            ),
+            round(
+                max_speed * common.SPEED_MB_SEC,
+                2,
+            ),
+            round(
+                max_speed * common.SPEED_MBIT_SEC,
+                2,
+            ),
+            round(
+                overall_speed
+                * common.SPEED_MB_SEC,
+                2,
+            ),
+            round(
+                overall_speed
+                * common.SPEED_MBIT_SEC,
+                2,
+            ),
+            round(
+                median_speed
+                * common.SPEED_MB_SEC,
+                2,
+            ),
+            round(
+                median_speed
+                * common.SPEED_MBIT_SEC,
+                2,
+            ),
+            round(
+                deviation
+                * common.SPEED_MB_SEC,
+                2,
+            ),
+            round(
+                deviation
+                * common.SPEED_MBIT_SEC,
+                2,
+            ),
+            f"v{VERSION}",
+        ]
+
+        overall = (
+            overall_headers,
+            overall_values,
+        )
+
+        csv_parser.csv_parser(
+            results,
+            csv_file,
+            overall,
+            filesize,
+        )
+
+    return 0
 
 
 if __name__ == "__main__":
