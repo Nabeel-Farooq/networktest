c06fbece4a0e5b89a5c3e3790d4e38c9d7129584
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-29 14:23:56 +0500

    Update uploadtester.py

--- bandwidth/uploadtester.py
+++ bandwidth/uploadtester.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 
 """
@@ -6,102 +6,169 @@
 """
 
 import errno
-from socket import error as SocketError
 import ftplib
-import ntpath
 import os
 import sys
 import time
+from pathlib import Path
+from socket import error as SocketError
 
 import common
 
 
-class UploadTester():
-    """Class to do upload speed tests."""
+class UploadTester:
+    """Class to perform upload speed tests."""
 
     DEFAULT_UPLOAD_COUNT = 1
     VERBOSE = True
-    host = ""
-    username = ""
-    password = ""
-    passive = False
-    current_dir = "/"
-    overall_time_elapsed = 0
 
-    __sizeWritten = 0
-    __filesize = 0
-    __filename = ""
-    __start = 0
-    __ftp = ftplib.FTP()
+    def __init__(self):
+        self.host = ""
+        self.username = ""
+        self.password = ""
+        self.passive = False
+        self.current_dir = "/"
+        self.overall_time_elapsed = 0
+
+        self._size_written = 0
+        self._filesize = 0
+        self._filename = ""
+        self._start = 0.0
+
+        self._ftp = ftplib.FTP()
 
     def cleanup(self):
         """Cleanup after test ends or it is cancelled."""
+
         try:
-            self.__ftp.delete(self.__filename)
-            self.__ftp.quit()
-        except (ftplib.error_temp, ftplib.error_perm, ftplib.error_reply):
+            if self._filename:
+                self._ftp.delete(self._filename)
+
+            self._ftp.quit()
+
+        except (
+            ftplib.error_temp,
+            ftplib.error_perm,
+            ftplib.error_reply,
+            OSError,
+        ):
             pass
-        self.__ftp.close()
+
+        finally:
+            try:
+                self._ftp.close()
+            except Exception:
+                pass
+
+    def connect(self):
+        """Connect and authenticate to FTP server."""
+
+        self._ftp.connect(self.host, 21)
+
+        try:
+            self._ftp.login(self.username, self.password)
+
+        except ftplib.error_perm as exc:
+            error_message = str(exc)
+
+            if "530" in error_message:
+                print("ERROR: Bad username or password.\n")
+                sys.exit(1)
+
+            raise
+
+        self._ftp.set_pasv(self.passive)
+        self._ftp.cwd(self.current_dir)
 
     def upload_file(self, upload_file):
-        """Return file size attribute.
+        """
+        Upload file to FTP server and return upload speed.
 
         Arguments:
-        upload_file -- Upload this file to FTP server
+        upload_file -- File path to upload
         """
-        chunk_size = 8192
-        dl_speed = 0
-        global n
-        self.__sizeWritten = 0
 
-        self.__ftp.connect(self.host, 21)
+        chunk_size = 8192
+        self._size_written = 0
+
+        upload_path = Path(upload_file)
+
+        self._filename = upload_path.name
+        self._filesize = upload_path.stat().st_size
+
+        self.connect()
+
+        self._start = time.time()
+
         try:
-            self.__ftp.login(self.username, self.password)
-        except ftplib.error_perm, e:
-            if "530 Login authentication failed" in e:
-                print "ERROR: Bad username or password."
-                print
-                sys.exit(1)
-        # Set passive mode
-        self.__ftp.set_pasv(self.passive)
-        self.__ftp.cwd(self.current_dir)
-        self.__filesize = os.path.getsize(upload_file)
-        # self.__start = time.clock()
-        self.__start = time.mktime(time.localtime())
-        file = open(upload_file, 'rb')
-        self.__filename = ntpath.basename(upload_file)
-        try:
-            self.__ftp.storbinary('STOR ' + self.__filename, file, chunk_size,
-                                  self.print_progress)
-        except SocketError as e:
-            if e.errno != errno.ECONNRESET:
-                raise  # Not error we are looking for
-            # pass
-            print "ERROR: Got connection reset, retring upload..."
-            n = n + 1
-        time_elapsed = (time.mktime(time.localtime()) - self.__start)
-        self.overall_time_elapsed = time_elapsed
-        dl_speed = self.__filesize/time_elapsed
-        return dl_speed
+            with upload_path.open("rb") as file:
+                self._ftp.storbinary(
+                    f"STOR {self._filename}",
+                    file,
+                    blocksize=chunk_size,
+                    callback=self.print_progress,
+                )
+
+        except SocketError as exc:
+            if exc.errno != errno.ECONNRESET:
+                raise
+
+            print("ERROR: Connection reset, retrying upload...")
+            return 0
+
+        finally:
+            self.overall_time_elapsed = round(
+                time.time() - self._start,
+                2,
+            )
+
+        if self.overall_time_elapsed <= 0:
+            return 0
+
+        upload_speed = (
+            self._filesize / self.overall_time_elapsed
+        )
+
+        return upload_speed
 
     def print_progress(self, chunk):
-        """Print upload progress.
+        """
+        Print upload progress.
 
         Arguments:
-        chunk -- Data being uploaded.
+        chunk -- Uploaded data chunk
         """
-        self.__sizeWritten += len(chunk)
-        done = int(50 * self.__sizeWritten / int(self.__filesize))
-        time_elapsed = (time.mktime(time.localtime()) - self.__start)
-        try:
-            dl_speed = self.__sizeWritten/time_elapsed
-            avr_speed = (dl_speed)/1000000
-            avr_speed_mbps = (dl_speed)*common.SPEED_MBIT_SEC
-            if self.VERBOSE:
-                sys.stdout.write("\r[%s%s] %s MB/s - %s Mbps" %
-                                 ('=' * done, ' ' * (50-done),
-                                  round(avr_speed, 2),
-                                  round(avr_speed_mbps, 2)))
-                sys.stdout.flush()
-        except ZeroDivisionError:
-            pass
+
+        self._size_written += len(chunk)
+
+        if self._filesize <= 0:
+            return
+
+        done = int(
+            50 * self._size_written / self._filesize
+        )
+
+        time_elapsed = time.time() - self._start
+
+        if time_elapsed <= 0:
+            return
+
+        upload_speed = self._size_written / time_elapsed
+
+        avg_speed_mb = upload_speed / 1_000_000
+        avg_speed_mbps = (
+            upload_speed * common.SPEED_MBIT_SEC
+        )
+
+        if self.VERBOSE:
+            sys.stdout.write(
+                "\r[%s%s] %s MB/s - %s Mbps"
+                % (
+                    "=" * done,
+                    " " * (50 - done),
+                    round(avg_speed_mb, 2),
+                    round(avg_speed_mbps, 2),
+                )
+            )
+
+            sys.stdout.flush()
