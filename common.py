98e54ffb94909e3939eb277c781f39fe158ff6f8
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-29 14:27:26 +0500

    Update common.py

--- common.py
+++ common.py
@@ -1,2 +1,12 @@
-SPEED_MBIT_SEC = 0.000008
-SPEED_MB_SEC = 0.000001
+#!/usr/bin/env python3
+# encoding: utf-8
+
+"""
+Common speed conversion constants.
+"""
+
+# Bytes/sec -> Megabytes/sec (MB/s)
+SPEED_MB_SEC = 1 / 1_000_000
+
+# Bytes/sec -> Megabits/sec (Mbps)
+SPEED_MBIT_SEC = 8 / 1_000_000
