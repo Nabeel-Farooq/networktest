b1cb4951a76f3e1995c1cf15687e11471ef63788
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-29 14:26:45 +0500

    Update csv_parser.py

--- bandwidth/csv_parser.py
+++ bandwidth/csv_parser.py
@@ -1,40 +1,61 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
-
-"""
-.. codeauthor:: Juan Luis Baptiste <juan.baptiste@gmail.com>
-"""
 
 import csv
 
+import common
+
+
+RESULT_HEADERS = [
+    "Sample#",
+    "File Size",
+    "Average Speed (MB/sec)",
+    "Average Throughput (Mbps)",
+]
+
 
 def csv_parser(results, csv_file, overall, filesize):
-    """Create csv file from test results.
+    """
+    Create CSV file from test results.
 
     Arguments:
-    results -- Array with the test results
-    csv_file --  Destination csv file
-    overall -- Array with overall test data (averages, deviations, etc)
-    filesize -- Size of the file being uploaded/downloaded
+    results -- Array with test results
+    csv_file -- Destination CSV file
+    overall -- Tuple containing overall headers and values
+    filesize -- Size of uploaded/downloaded file
     """
-    with open(csv_file, 'wb') as myfile:
-        wr = csv.writer(myfile)
-        # TODO: Add date of test and test info to file
-        overall_header = overall[0]
-        overall_speeds_csv = overall[1]
-        wr.writerow(overall_header)
-        wr.writerow(overall_speeds_csv)
-        wr.writerow([])
-        header = ["Sample#",
-                  "File Size",
-                  "Average Speed (MB/sec)",
-                  "Average Throughput (Mbps)"]
-        wr.writerow(header)
-        n = 1
-        for result in results:
-            row = [n,
-                   filesize,
-                   round(result*0.000001, 2),
-                   round(result*0.000008, 2)]
-            wr.writerow(row)
-            n += 1
+
+    overall_header, overall_values = overall
+
+    with open(
+        csv_file,
+        "w",
+        newline="",
+        encoding="utf-8",
+    ) as file:
+
+        writer = csv.writer(file)
+
+        writer.writerow(overall_header)
+        writer.writerow(overall_values)
+        writer.writerow([])
+
+        writer.writerow(RESULT_HEADERS)
+
+        for sample_num, result in enumerate(
+            results,
+            start=1,
+        ):
+
+            writer.writerow([
+                sample_num,
+                filesize,
+                round(
+                    result * common.SPEED_MB_SEC,
+                    2,
+                ),
+                round(
+                    result * common.SPEED_MBIT_SEC,
+                    2,
+                ),
+            ])
